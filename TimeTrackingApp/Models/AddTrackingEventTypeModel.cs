7e1ca6165330b4bd6e838384c8f3c6b68b19fcbb
aliakseiredzko <aliakseiredzko@gmail.com>
2022-12-15 10:30:32 +0100

    Add EditFrom

---
+++ TimeTrackingApp/Models/AddTrackingEventTypeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTrackingApp.Models
+{
+    public class AddTrackingEventTypeModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Shouldn't be empty!")]
+        public string EventType { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Description { get; set; }
+    }
+}
