deffea94961e2c95f2959ab2654f184525b935e5
aliakseiredzko <aliakseiredzko@gmail.com>
2023-01-11 16:17:06 +0100

    Add logging behaviour for mediatr pipeline

---
+++ TimeTrackingApp.Infrastructure/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace TimeTrackingApp.Infrastructure.Behaviours
+{
+    public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
+
+        public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Start processing {TRequest}", typeof(TRequest));
+            TResponse response = await next();
+            _logger.LogDebug("End processing {TRequest}", typeof(TRequest));
+            return response;
+        }
+    }
+}
--- TimeTrackingApp.Infrastructure/Extensions/MediatRExtensions.cs
+++ TimeTrackingApp.Infrastructure/Extensions/MediatRExtensions.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using TimeTrackingApp.Infrastructure.Behaviours;
 
 namespace TimeTrackingApp.Infrastructure.Extensions
 {
@@ -8,6 +9,7 @@ namespace TimeTrackingApp.Infrastructure.Extensions
         public static void AddMediatRDependencies(this IServiceCollection services)
         {
             services.AddMediatR(typeof(MediatRExtensions));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));           
         }
     }
 }
